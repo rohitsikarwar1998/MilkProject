1188e09c68ae97e361929094a1b3ea3c8c0c92e9
Rohit Sikarwar <rohitsikarwar1998@gmail.com>
2021-07-18 17:53:25 +0530

    initial project setup

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {StatusBar, Text, useColorScheme, View} from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: antiquewhite;
+`;
+
+const TitleContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0 10px #ccc;
+  background-color: white;
+  padding: 20px;
+  border-radius: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+`;
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Hello World</Text>
-    </View>
+    <Container>
+      <TitleContainer>
+        <Title>Hello World</Title>
+      </TitleContainer>
+    </Container>
   );
 };
 
